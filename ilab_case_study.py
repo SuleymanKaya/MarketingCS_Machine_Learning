55a40abc0bd4f3f3baffb2b77c3d39e249e0b299
Süleyman Kaya <32798253+SuleymanKaya@users.noreply.github.com>
2020-08-10 01:03:06 +0300

    Update ilab_case_study.py

--- ilab_case_study.py
+++ ilab_case_study.py
@@ -12,13 +12,18 @@ from sklearn.preprocessing import LabelEncoder, OneHotEncoder, StandardScaler
 from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.svm import SVC, SVR
 from sklearn.metrics import confusion_matrix, accuracy_score
+from scipy import stats
 
+#Kullanılacak Ham Verininin Yüklenmesi
 all_data = pd.read_csv("term-deposit-marketing-2020.csv")
 
 #Ham Verinin Analizi
+all_data.info()
+print("-----------------------------------------------------------------")
 all_data_types= all_data.dtypes
 all_data_details= all_data.describe()
 all_data_details2= all_data.describe(include="all")
+
 
 #Eksik Verilerin En Sık Bulunan Veriler İle Değiştirilmesi
 all_data.replace("unknown", np.nan, inplace= True)
@@ -32,11 +37,13 @@ Eksik (unknown) Veri Sayısı - job: 235 | education: 1531 | contact:12765
 #Eksik Verilerin Olup Olmadığının Tespiti (Varsa "True" olarak veri setinde gösterir)
 missing_data = all_data.isnull()
 
-#Her Alan İçin Eksik Verilen Gösterimi
+#Her Alan İçin Eksik Verilerin Gösterimi
 for column in missing_data.columns.values.tolist():
     print(column)
     print(missing_data[column].value_counts())
     print("")
+
+print("-----------------------------------------------------------------")
 
 #Kopyalama İşlemi
 all_data2 = all_data
@@ -61,9 +68,39 @@ all_data4 = pd.DataFrame(data = all_data4, index = range(40000), columns=['divor
 #Hazırlanan Verilen Birleştirme İşlemi
 all_data5 = pd.concat([all_data3, all_data4], axis=1)
 
+
+#Outliers Verilerin Sistemden Çıkartılması İşlemi
+for column in all_data5:
+    all_data5['z_score']=stats.zscore(all_data5[column])
+    all_data5.rename(columns={'z_score': 'z_score of '+column}, inplace=True)
+    all_data5 = all_data5[all_data5['z_score of '+column].abs()<=5]
+    all_data5 = all_data5.drop('z_score of '+column, 1)
+
 #Bağımlı ve Bağımsız Değişkenlerin Oluşturulması
 x_input = all_data5.drop('y',1)
-y_output = all_data5['y'] 
+y_output = all_data5['y']
+
+#Olasılık Değeri (p-value) ve Korelasyon Değerine Bağlı Olarak Geri Eleme (Backward Elimination) Yapma    
+#p_and_corr Verisi Müşterilerin Ürünü En Çok Almasında Etkili Özellikler Kod Bloğunda Kullanılacaktır
+unrelated_data = []
+p_and_corr = pd.DataFrame(columns=["p_value","pearson_coef"])
+
+for column in x_input:
+    pearson_coef, p_value = stats.pearsonr(x_input[column], y_output)
+    print('Alan Adı: ', column)
+    print("p_value: ", p_value, "|", "pearson_coef: ", pearson_coef)
+    p_and_corr = p_and_corr.append({'p_value':p_value , 'pearson_coef':pearson_coef}, ignore_index=True)
+    unrelated_data_check = p_value<0.05 and abs(pearson_coef)>0.01
+    if unrelated_data_check == False:
+        unrelated_data.append(column)
+
+p_and_corr.index = x_input.columns
+
+#İlişki Çok Az Olan Alanların Veri Setinden Çıkarılması İşlemi
+x_input = x_input.drop(unrelated_data,1)
+ 
+print("-----------------------------------------------------------------")
+
 
 #Veri Setinin Eğitim ve Test Kümelerine Bölünmesi
 x_train, x_test, y_train, y_test = train_test_split(x_input, y_output, test_size = 0.33, random_state = 0)
@@ -72,6 +109,7 @@ x_train, x_test, y_train, y_test = train_test_split(x_input, y_output, test_size
 sc = StandardScaler()
 x_train = sc.fit_transform(x_train)
 x_test = sc.transform(x_test)
+
 
 #SVM ile Öğrenme
 classifier = SVC(kernel = 'rbf', random_state = 0)
@@ -82,12 +120,15 @@ y_pred = classifier.predict(x_test)
 
 #Confusion Matrix
 cm = confusion_matrix(y_test, y_pred)
+print("-----------------------------------------------------------------")
 print(cm)
 print('Accuracy Score:', accuracy_score(y_test, y_pred)*100)
-
+print("-----------------------------------------------------------------")
 #5-Fold Cross Validation
 success = cross_val_score(estimator = classifier, X=x_train, y=y_train , cv = 5)
+print("-----------------------------------------------------------------")
 print("ML Model Success Rate: ", success.mean()*100)
+print("-----------------------------------------------------------------")
 #print(success.std())
 
 """
@@ -96,16 +137,28 @@ Case Study Success Metric : Hit %81 or above accuracy by evaluating with 5-fold 
 Result Success Metrics: Accuracy Score and ML Model Success Rate are "%93".
 """
 
-#Abone Olma Olasılığı En Yüksek 10 Müşteri = interested_10customers
+
+#Abone Olma Olasılığı En Yüksek Müşteriler (Büyüktün Küçüğe Doğru) = interested_10customers
 dvr_reg= SVR(kernel="rbf")
 dvr_reg.fit(x_train, y_train)
 y_pred2 = dvr_reg.predict(x_test)
-interested_10customers = pd.DataFrame(data = y_pred2, columns=['y_tahmin']).sort_values('y_tahmin', ascending=False).head(10)
+interested_10customers = pd.DataFrame(data = y_pred2, columns=['Potantial_Customers']).sort_values('Potantial_Customers', ascending=False)
 
 #Müşterilerin Ürünü Satın Almasında En Çok Etkili Olan Özellikler
 #Sırasıyla En Çok Etkileyen 3 Parametre: 1.DURATION | 2.MARRIED | 3.HOUSING
-correlation = all_data5.corr()
-related_field = correlation[correlation.index.str.startswith('y')].abs().unstack().sort_values(ascending=False).head(4)
+p_and_corr = p_and_corr.abs()
+p_values_data = p_and_corr.iloc[:,0].sort_values(ascending=True)
+print("Related Faetures1:\n", p_values_data.head(3))
+corr_data = p_and_corr.iloc[:,1].sort_values(ascending=False)
+print("Related Faetures2:\n", corr_data.head(3))
+print("-----------------------------------------------------------------")
+
+#Gerçek ve Tahmin Edilen Verilerin Grafiksel Olarak Karşılaştırılması
+y_pred = pd.Series(y_pred).rename('y')
+y_test.plot(kind='hist', x='index', y='y', color='red', figsize=(12,8))
+plt.show()
+y_pred.plot(kind='hist', x='index', y='y', color='blue', figsize=(12,8))
+plt.show()
 
 """
 Note2:
@@ -113,3 +166,4 @@ Bonuses:
 - We are also interested in finding customers who are more likely to buy the investment product. Determine the segment(s) of customers our client should prioritize.
 - What makes the customers buy? Tell us which feature we should be focusing more on.
 """
+
